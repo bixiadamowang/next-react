92d2ffaa731130aa7d4d667508c6e3943bd60b5a
hzc <384716313@qq.com>
2020-01-15 10:57:04 +0800

    简单路由

---
+++ next-react/pages/HanA.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+function HanA() {
+  return (
+    <div>
+      <div>笔下魔王A</div>
+      <Link href="/"><a>回首页</a></Link>
+    </div>
+  );
+}
+
+export default HanA;
---
+++ next-react/pages/HanB.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+function HanB() {
+  return (
+    <div>
+      <div>笔下魔王B</div>
+      <Link href="/"><a>回首页</a></Link>
+    </div>
+  );
+}
+
+export default HanB;
