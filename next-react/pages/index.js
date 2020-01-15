d9c0e0c0c4ad2c478ac4ff687ec5acee42470cdc
hzc <384716313@qq.com>
2020-01-15 11:11:38 +0800

    Router方式路由

--- next-react/pages/index.js
+++ next-react/pages/index.js
@@ -1,20 +1,33 @@
 import React from "react";
+import Router from "next/router";
 import Link from "next/link";
 
-const Home = () => (
-  <>
-    <div>首页</div>
-    <div>
-      <Link href="/hanA">
-        <a>去HanA</a>
-      </Link>
-    </div>
-    <div>
-      <Link href="/hanB">
-        <a>去HanB</a>
-      </Link>
-    </div>
-  </>
-);
+const Home = () => {
+  function goA() {
+    Router.push("/hanA");
+  }
+  return (
+    <>
+      <div>首页</div>
+      <div>
+        <Link href="/hanA">
+          <a>去HanA</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/hanB">
+          <a>去HanB</a>
+        </Link>
+      </div>
+      <div>
+        <button
+          onClick={goA}
+        >
+          Router方式去HanA
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Home;
